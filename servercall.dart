762e7e833df547ff7b517b7a13a3cc7a7c05ae57
paras <parasarora1221@gmail.com>
2019-07-08 22:21:23 +0530

    play pause ready

--- servercall.dart
+++ servercall.dart
@@ -13,21 +13,27 @@ class MySong {
 }
 
 class ServerCall extends StatefulWidget {
+
   @override
   _ServerCallState createState() => _ServerCallState();
+
 }
 
 class _ServerCallState extends State<ServerCall> {
+  
   List<MySong> songList = [];
+  bool isPlay = true;
+
   callServer() async {
     var result = await http
-        .get('https://itunes.apple.com/search?term=jack+johnson&limit=10');
+        .get('https://itunes.apple.com/search?term=sidhuMoosewala'
+        '&limit=100');
  // print("paras first result is $result");
     var obj = json.decode(result.body);
 // in obj we get two things one is resultCount and other is results
    // print("Obj is $obj");
     var arr = obj['results'];
-    print( "paras obj is $arr");
+ //   print( "paras obj is $arr");
 
     List<MySong> songList2 = [];
 
@@ -39,9 +45,9 @@ class _ServerCallState extends State<ServerCall> {
       songList2.add(song);
     }
 
-    print("Now SOngList is ");
+ //   print("Now SOngList is ");
     for (int i = 0; i < songList2.length; i++) {
-      print("Song is ${songList2[i].trackName}");
+   //   print("Song is ${songList2[i].trackName}");
     }
     setState(() {
       songList = songList2;
@@ -50,6 +56,7 @@ class _ServerCallState extends State<ServerCall> {
 
   _funReturnListTitle()
   {
+    
     return  Container(
       child: ListView.builder(
           itemCount: songList.length,
@@ -66,13 +73,18 @@ class _ServerCallState extends State<ServerCall> {
 
                 ),
                   flex: 8,
+
                 ),
-                Expanded(child:
-                IconButton(
-                  icon: Icon(Icons.play_arrow),
+                Expanded(
+                  child: IconButton(
+                //  icon: Icon( Icons.play_arrow),
                   padding: EdgeInsets.all(7.0),
+                 icon: Icon(isPlay? Icons.play_arrow:Icons.pause),
                   onPressed: () {
-                  _playSong(index);
+
+                  //  print("flow before pressed $boola");
+                 bool bool1= _playSong(index ,);
+                 print("after pressing button $bool1");
 
                   },
                 ),
@@ -88,12 +100,28 @@ class _ServerCallState extends State<ServerCall> {
   }
 
   // fun Play Song
-       _playSong( index) async {
-    print("Play Song Called");
-    AudioPlayer audioPlayer = AudioPlayer();
-    await audioPlayer.play(
-        songList[index].previewUrl);
-  }
+        _playSong( index ) async {
+         AudioPlayer audioPlayer = AudioPlayer();
+         print("flow is before if $isPlay");
+
+         if (isPlay ) {
+           print("flow is in if condition $isPlay");
+           await audioPlayer.play(
+               songList[index].previewUrl);
+             isPlay=false;
+          // print("flow is in if condition 2 $boola");
+
+         }
+         else
+           {
+             await audioPlayer.stop();
+             print("now flow is in else statement $isPlay");
+              isPlay=true;
+             print("now flow is in else statement2 $isPlay");
+
+           }
+
+       }
 
 
 
@@ -102,58 +130,5 @@ class _ServerCallState extends State<ServerCall> {
   Widget build(BuildContext context) {
     callServer();
     return _funReturnListTitle();
-
-
-    /*  Container(
-      child: ListView.builder(
-          itemCount: songList.length,
-          itemBuilder: (context, index) {
-            return Row(
-
-              children: <Widget>[
-             Expanded(child:
-              ListTile(
-             // leading: Icon(Icons.email),
-              title: Text(songList[index].trackName),
-              subtitle: Text(songList[index].artistName),
-           //  trailing: Icon(Icons.play_arrow),
-
-            ),
-               flex: 8,
-             ),
-                Expanded(child:
-                IconButton(
-                  icon: Icon(Icons.play_arrow),
-                  padding: EdgeInsets.all(7.0),
-                  onPressed: () {
-                    
-
-                  },
-                ),
-                  flex: 1,
-                ),
-
-            ],
-            );
-
-      /*
-              ListTile(
-              title: Text(
-                songList[index].trackName,
-                style: TextStyle(fontSize: 40),
-              ),
-            );
-
-            */
-          }),
-//      child: RaisedButton(
-//        onPressed: () {
-//          callServer();
-//        },
-//        child: Text('Call Server'),
-//      ),
-    );
-
-    */
   }
 }
