bf9d99707eb81c9c2f3255509c1f4f8912e7c322
paras <parasarora1221@gmail.com>
2019-07-12 12:52:17 +0530

    v1

---
+++ fakeDataJsonStatic.dart
@@ -0,0 +1,12 @@
+class fakeDataJsonStatic
+{
+  String imageUrl;
+  int productPrice;
+  int quantityAvailabe;
+  String productName;
+  int id;
+
+  fakeDataJsonStatic(this.imageUrl, this.productPrice, this.quantityAvailabe,
+      this.productName,this.id);
+
+}
