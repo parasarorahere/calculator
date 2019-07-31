63492feea1c4299eb51ffc55319bd5b56c4a10f6
paras <parasarora1221@gmail.com>
2019-07-31 10:18:09 +0530

    exp jsonlist 1

---
+++ jsonList.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' ;
+
+import 'dart:convert';
+import 'dataModel.dart';
+
+
+class jsonList extends StatefulWidget {
+  String authToken;
+
+  jsonList(token) {
+    this.authToken = token;
+  }
+
+
+  @override
+  _jsonListState createState() => _jsonListState(
+  );
+}
+
+
+class _jsonListState extends State<jsonList> {
+
+ // List<Sr> popupmenu;
+
+  List<String> selectedLocations = List();
+  MasterData masterData;
+
+  _makeGetRequest(String token) async {
+
+    print("Master Data called $token");
+    // make GET request
+    String url = 'http://192.168.0.106/EXPAudit64/api/users/GetMasterData/1';
+    Map<String, String> headers = {"Content-Type": "application/json","timezone": "GMT+5:30","offset":"5.5","Auth_Token":token};
+    Response response = await get(url,headers: headers);
+    // sample info available in response
+    var bodyResponse = response.body;
+ //   print("body response is $bodyResponse");
+
+    var  data = json.decode(response.body);
+    masterData = MasterData(data["data"]);
+    setState(() {
+    masterData: masterData;
+  });
+  }
+
+  Widget build(BuildContext context) {
+
+    if (masterData == null) {
+      _makeGetRequest(widget.authToken);
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("EXP",style: TextStyle(fontSize: 20),),
+
+      ),
+      body: Container(
+
+        child: ListView.builder
+          (
+            itemCount: (masterData != null) ? masterData.locationlevels.length : 0,
+            itemBuilder: (BuildContext ctxt, int index) {
+              return   ListTile(
+                onTap: (){} ,
+                title: Text(masterData.locationlevels[index].levelName,style: TextStyle(fontSize: 30),),
+                subtitle: ,
+
+              );
+            }
+        ),
+
+      )
+    );
+  }
+}
